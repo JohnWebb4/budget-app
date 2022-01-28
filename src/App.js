a7e19413159d00dba89708a0abca730da8509d1c
John Webb <john.webb94@gmail.com>
2022-01-28 00:22:52 -0700

    Add botton tabs with react navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import DatabaseProvider from '@nozbe/watermelondb/DatabaseProvider';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {database} from './model';
 import {AddPage} from './pages/AddPage';
+import {HistoryPage} from './pages/HistoryPage';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <DatabaseProvider database={database}>
-      <AddPage />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Add" component={AddPage} />
+          <Tab.Screen name="History" component={HistoryPage} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </DatabaseProvider>
   );
 };
